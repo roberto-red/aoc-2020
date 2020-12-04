24e19172c15d16544ca91b5e8e51fc1e24793e88
Roberto Redradix <roberto@redradix.com>
2020-12-04 16:34:34 +0100

    Green: AoC 4: invalid passport (missing ecl)

--- day4.part1.js
+++ day4.part1.js
@@ -3,7 +3,9 @@ const path = require('path')
 
 const input = fs.readFileSync(path.join(__dirname, 'day4.input.txt'), 'utf-8')
 
-const isValidPassport = passport => passport.includes('hgt') && passport.includes('byr')
+const isValidPassport = passport => passport.includes('hgt')
+  && passport.includes('byr')
+  && passport.includes('ecl')
 
 const countPassports = (input) => {
   const passports = input.split('\n\n')
