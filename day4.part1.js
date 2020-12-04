cd4ffab920b2d4020438e926509a3011be70a859
Roberto Redradix <roberto@redradix.com>
2020-12-04 16:06:31 +0100

    Green: AoC 4: invalid passport (missing byr)

--- day4.part1.js
+++ day4.part1.js
@@ -4,7 +4,7 @@ const path = require('path')
 const input = fs.readFileSync(path.join(__dirname, 'day4.input.txt'), 'utf-8')
 
 const countPassports = (input) => {
-  return input.includes('hgt') ? 1 : 0
+  return input.includes('hgt') && input.includes('byr') ? 1 : 0
 }
 
 module.exports = {
