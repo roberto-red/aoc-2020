ab9e796fb1f8f6a612c63ea3c4b1ba8cc4042949
Roberto Redradix <roberto@redradix.com>
2020-12-04 16:04:44 +0100

    Red: AoC 4: invalid passport

--- day4.part1.test.js
+++ day4.part1.test.js
@@ -8,10 +8,18 @@ byr:1937 iyr:2017 cid:147 hgt:183cm`
     expect(countPassports(inputMagico)).toEqual(1)
   })
 
-  it('does not count invalid passports', () => {
+  it('does not count invalid passports (missing hgt)', () => {
     const inputMagico = `iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
 hcl:#cfa07d byr:1929`
 
     expect(countPassports(inputMagico)).toEqual(0)
   })
+
+    it('does not count invalid passports (missing byr)', () => {
+    const inputMagico = `hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in`
+
+    expect(countPassports(inputMagico)).toEqual(0)
+  })
+
 })
