ae46f145108bffbd1aff70ffc8aed80a417ef37b
Roberto Redradix <roberto@redradix.com>
2020-12-04 16:22:58 +0100

    Refactor: AoC 4: extract isValidPassport

--- day4.part1.js
+++ day4.part1.js
@@ -3,11 +3,13 @@ const path = require('path')
 
 const input = fs.readFileSync(path.join(__dirname, 'day4.input.txt'), 'utf-8')
 
+const isValidPassport = passport => passport.includes('hgt') && passport.includes('byr')
+
 const countPassports = (input) => {
   const passports = input.split('\n\n')
 
-  return (passports[0].includes('hgt') && passports[0].includes('byr') ? 1 : 0)
-    + (passports[1] !== undefined && passports[1].includes('hgt') && passports[1].includes('byr') ? 1 : 0)
+  return (isValidPassport(passports[0]) ? 1 : 0)
+    + (passports[1] !== undefined && isValidPassport(passports[1]) ? 1 : 0)
 }
 
 module.exports = {
