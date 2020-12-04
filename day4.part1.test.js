73f4f02340cf76322284b4573f2159cbe191dfcc
Roberto Redradix <roberto@redradix.com>
2020-12-04 16:34:25 +0100

    Red: AoC 4: invalid passport (missing ecl)

--- day4.part1.test.js
+++ day4.part1.test.js
@@ -15,6 +15,14 @@ hcl:#cfa07d byr:1929`
     expect(countPassports(inputMagico)).toEqual(0)
   })
 
+  it('does not count invalid passports (missing ecl)', () => {
+    const inputMagico = `pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm`
+
+    expect(countPassports(inputMagico)).toEqual(0)
+  })
+
+
   it('does not count invalid passports (missing byr)', () => {
     const inputMagico = `hcl:#cfa07d eyr:2025 pid:166559648
 iyr:2011 ecl:brn hgt:59in`
