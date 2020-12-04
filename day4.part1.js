c91d003e8249dd816ec8982825719966bd69d704
Roberto Redradix <roberto@redradix.com>
2020-12-04 15:56:38 +0100

    Fail: AoC 4

---
+++ day4.part1.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const path = require('path')
+
+const input = fs.readFileSync(path.join(__dirname, 'day4.input.txt'), 'utf-8')
+
+const countPassports = () => {
+  return false
+}
+
+module.exports = {
+  countPassports,
+}
---
+++ day4.part1.test.js
@@ -0,0 +1,10 @@
+const { countPassports } = require('./day4.part1.js')
+
+describe('count passports', () => {
+  it('counts a single valid passport', () => {
+    const inputMagico = `ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm`
+
+    expect(countPassports(inputMagico)).toEqual(1)
+  })
+})
