554c0a79c1052c7b32963238a980d26d6d5697a4
Roberto Redradix <roberto@redradix.com>
2020-12-04 17:03:02 +0100

    Green: AoC 4: invalid hgt

--- day4.part2.js
+++ day4.part2.js
@@ -4,6 +4,7 @@ const REQUIRED_FIELDS = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
 const isValidPassport = passport => {
   for (const field of REQUIRED_FIELDS) {
     if (!passport.includes(field)) return false
+    if (field === 'hgt' && !/\d+(cm|in)/.test(passport)) return false
   }
   return true
 }
