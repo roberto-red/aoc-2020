71477c620e6f7f32489749c41ef743c150eb3bad
Roberto Redradix <roberto@redradix.com>
2020-12-04 16:37:53 +0100

    Refactor: AoC 4: is valid passports

--- day4.part1.js
+++ day4.part1.js
@@ -1,11 +1,16 @@
 const fs = require('fs')
 const path = require('path')
 
+const REQUIRED_FIELDS = ['hgt', 'byr', 'ecl']
+
 const input = fs.readFileSync(path.join(__dirname, 'day4.input.txt'), 'utf-8')
 
-const isValidPassport = passport => passport.includes('hgt')
-  && passport.includes('byr')
-  && passport.includes('ecl')
+const isValidPassport = passport => {
+  for (const field of REQUIRED_FIELDS) {
+    if (!passport.includes(field)) return false
+  }
+  return true
+}
 
 const countPassports = (input) => {
   const passports = input.split('\n\n')
