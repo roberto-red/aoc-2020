cd392fa870ed142739d3a1f84da104591a42f800
Roberto Redradix <roberto@redradix.com>
2020-12-04 16:19:11 +0100

    Green: AoC 4: count multiple passports

--- day4.part1.js
+++ day4.part1.js
@@ -4,7 +4,10 @@ const path = require('path')
 const input = fs.readFileSync(path.join(__dirname, 'day4.input.txt'), 'utf-8')
 
 const countPassports = (input) => {
-  return input.includes('hgt') && input.includes('byr') ? 1 : 0
+  const passports = input.split('\n\n')
+
+  return (passports[0].includes('hgt') && passports[0].includes('byr') ? 1 : 0)
+    + (passports[1] !== undefined && passports[1].includes('hgt') && passports[1].includes('byr') ? 1 : 0)
 }
 
 module.exports = {
