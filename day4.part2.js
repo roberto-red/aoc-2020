5c663f3f5e020792e7c2ea3677ae8a310db531d0
Roberto Redradix <roberto@redradix.com>
2020-12-06 14:04:05 +0100

    Solve AoC 4 part 2

--- day4.part2.js
+++ day4.part2.js
@@ -1,23 +1,77 @@
 // https://adventofcode.com/2020/day/4#part2
 
-const REQUIRED_FIELDS = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+const every = validations => value => validations.every(v => v(value))
 
+const isRequired = value => value !== undefined
 
-const isValidPassport = passport => {
-  for (const field of REQUIRED_FIELDS) {
-    if (!passport.includes(field)) return false
-    if (field === 'hgt' && !/\d+(cm|in)/.test(passport)) return false
-    if (field === 'pid' && !/anything/.test(passport)) return false
-  }
-  return true
+const fixedLength = length => value => value.length === length
+
+const VALIDATE_FIELDS = {
+  byr: every([
+    isRequired,
+    fixedLength(4),
+    value => /\d{4}/.test(value)
+      && value >= '1920'
+      && value <= '2002',
+  ]),
+  cid: value => true,
+  iyr: every([
+    isRequired,
+    fixedLength(4),
+    value => /\d{4}/.test(value)
+    && value >= '2010'
+    && value <= '2020',
+  ]),
+  eyr: every([
+    isRequired,
+    fixedLength(4),
+    value => /\d{4}/.test(value)
+    && value >= '2020'
+    && value <= '2030',
+  ]),
+  hgt: every([
+    isRequired,
+    value => /\d+(cm|in)/.test(value),
+    value => {
+      const [, number, units] = value.match(/(\d+)(cm|in)/)
+      if (units === 'cm' && (number < '150' || number > '193')) return false
+      if (units === 'in' && (number < '59' || number > '76')) return false
+      return true
+    }
+  ]),
+  hcl: every([
+    isRequired,
+    fixedLength(7),
+    value => /#[0-9a-f]{6}/.test(value),
+  ]),
+  ecl: every([
+    isRequired,
+    value => ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(value),
+  ]),
+  pid: every([
+    isRequired,
+    fixedLength(9),
+    value => /\d{9}/.test(value),
+  ]),
 }
 
-const countPassports = (input) => {
-  const passports = input.split('\n\n')
-
-  return passports.filter(isValidPassport).length
+const parsePassport = rawPassport => {
+  const entries = rawPassport
+    .split(/\s/)
+    .map(field => field.split(':'))
+  return Object.fromEntries(entries)
 }
+
+const isValidPassport = passport => Object.entries(VALIDATE_FIELDS)
+  .every(([field, validate]) => validate(passport[field]))
+
+const countPassports = (input) => input
+  .split('\n\n')
+  .map(parsePassport)
+  .filter(isValidPassport)
+  .length
 
 module.exports = {
   countPassports,
+  VALIDATE_FIELDS,
 }
--- day4.part2.test.js
+++ day4.part2.test.js
@@ -1,21 +1,69 @@
 const fs = require('fs')
 const path = require('path')
-const { countPassports } = require('./day4.part2.js')
+const { countPassports, VALIDATE_FIELDS: validations} = require('./day4.part2.js')
 
 const input = fs.readFileSync(path.join(__dirname, 'day4.input.txt'), 'utf-8')
 
-describe('count passports AoC 4 part 2', () => {
-  it('does not count an invalid passport (invalid hgt)', () => {
-    const inputMagico = `eyr:1972 cid:100
-hcl:#18171d ecl:amb hgt:170 pid:123456789 iyr:2018 byr:1926`
+const allInvalidInput = `eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
 
-    expect(countPassports(inputMagico)).toEqual(0)
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007`
+
+const allValidInput = `pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719`
+
+
+describe('field validation AoC part 2', () => {
+  [
+    { field: 'byr', valid: true , value: '2002' },
+    { field: 'byr', valid: false, value: '2003' },
+    { field: 'hgt', valid: true , value: '60in' },
+    { field: 'hgt', valid: true , value: '190cm' },
+    { field: 'hgt', valid: false, value: '190in' },
+    { field: 'hgt', valid: false, value: '190' },
+    { field: 'hcl', valid: true , value: '#123abc' },
+    { field: 'hcl', valid: false, value: '#123abz' },
+    { field: 'hcl', valid: false, value: '123abc' },
+    { field: 'ecl', valid: true , value: 'brn' },
+    { field: 'ecl', valid: false, value: 'wat' },
+    { field: 'pid', valid: true , value: '000000001' },
+    { field: 'pid', valid: false, value: '0123456789' },
+  ].forEach(({ field, value, valid }) => {
+    it(`correctly validates ${field} with value ${value}`, () => {
+      expect(validations[field](value)).toBe(valid)
+    })
+  })
+})
+
+describe('count passports AoC 4 part 2', () => {
+  it('does not count invalid passports', () => {
+    expect(countPassports(allInvalidInput)).toEqual(0)
   })
 
-  it('does not count an invalid passport (invalid pid)', () => {
-    const inputMagico = `eyr:1972 cid:100
-hcl:#18171d ecl:amb hgt:170cm pid:186cm iyr:2018 byr:1926`
+  it('counts valid passports', () => {
+    expect(countPassports(allValidInput)).toEqual(4)
+  })
 
-    expect(countPassports(inputMagico)).toEqual(0)
+  it('solves AoC 4 input', () => {
+    expect(countPassports(input)).toEqual(140)
   })
 })
